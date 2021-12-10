6701fb7003b7344b035b87d38b54c07be42bb019
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2021-12-10 15:54:31 +0100

    tentatvo ciclo for

--- myscript.js
+++ myscript.js
@@ -2,3 +2,34 @@
 // richiamo tramite id , div container
 const contenitore = document.getElementById("contenitore");
 
+// creo ciclo "for" per assegnare classi .box . fizz .buzz
+
+for(let i=1; i<=100; i++){
+
+    if(i%3 == 0){
+        i="Fizz";
+        console.log("i")
+        contenitore.innerHTML+='<div class="box fizz">' + i + '</div>';
+    
+    }else if(i%5 == 0){
+        i="Buzz";
+        console.log("i")
+        contenitore.innerHTML+='<div class="box Buzz">' + i + '</div>';
+    
+    }else if ((i%3 == 0)&&(i%5 == 0)) {
+        i="FizzBuzz";
+        console.log("i")
+        contenitore.innerHTML+='<div class="box fizzBuzz">' + i + '</div>';
+    
+    }else {
+        console.log("i")
+        contenitore.innerHTML+='<div class="box">' + i + '</div>';
+    }
+
+
+
+
+
+
+
+}
